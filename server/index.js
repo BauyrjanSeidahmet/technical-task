f667d3c9ba39e6c31b41808d6c49b0822bdd6676
BauyrjanSeidakhmet <bauke.10.bs@gmail.com>
2021-11-11 15:40:51 +0600

    added resolver

--- server/index.js
+++ server/index.js
@@ -2,13 +2,24 @@ const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const cors = require('cors')
 const schema = require('./schema')
+const users = [{id: 1, email: 'Bauka@mail.ru', age: 25}]
 
 const app = express()
 app.use(cors())
 
+const root = {
+    getAllUsers: () => {
+        return users
+    },
+    getUser: ({id}) => {
+        return users.find(user => user.id == id)
+    }
+}
+
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
-    schema
+    schema,
+    rootValue: root
 }))
 
 app.listen(5000, () => console.log('server started on port 5000'))
