69f4b1cc0f82ce592f2bc39c7f9fa54f105bdba8
BauyrjanSeidahmet <bauke.10.bs@gmail.com>
2021-11-14 10:25:09 +0600

    created register form

---
+++ client/src/containers/Register/Register.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import FormElement from "../../components/UI/Form/FormElement/FormElement";
+import UserForm from "../../components/UserForm/UserForm";
+import { createUser } from '../../store/actions/usersActions';
+
+
+const Register = () => {
+    const dispatch = useDispatch();
+    const error = useSelector(state => state.users.registerError);
+
+    const [state, setState] = useState({
+        email: "",
+        password: "",
+        age: ""
+    });
+
+    const inputChangeHandler = e => {
+        const {name, value} = e.target;
+        setState(prevState => {
+            return {...prevState, [name]: value};
+        });
+    };
+
+    const submitFormHandler = async e => {
+        e.preventDefault();
+        await dispatch(createUser({...state}));
+    };
+
+    const getFieldError = fieldName => {
+        try {
+            return error.errors[fieldName].message;
+        } catch(e) {
+            return undefined;
+        }
+    };
+
+    return (
+        <UserForm
+            onSubmit={submitFormHandler}
+            title="Sign Up"
+        >
+            <FormElement
+                name="email"
+                value={state.email}
+                onChange={inputChangeHandler}
+                error={getFieldError("email")}
+                label="Email"
+                type="email"
+            />
+            <FormElement
+                name="password"
+                value={state.password}
+                onChange={inputChangeHandler}
+                error={getFieldError("password")}
+                label="Password"
+                type="password"
+            />
+               <FormElement
+                name="age"
+                value={state.age}
+                onChange={inputChangeHandler}
+                error={getFieldError("age")}
+                label="Your Age"
+                type="text"
+            />
+        </UserForm>
+    );
+}
+
+export default Register;
