d9a7dd56f7c1143f6ed10d2f302aec0f91bd0ef3
BauyrjanSeidakhmet <bauke.10.bs@gmail.com>
2021-11-15 11:10:16 +0600

    changed db url

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ module.exports = {
   rootPath,
   db: {
     name: "users",
-    url: "mongodb://localhost/"
+    url: "mongodb+srv://admin:admin@cluster0.x67n5.mongodb.net/techtask?retryWrites=true&w=majority"
   },
   getDbUrl() {
     return this.db.url + this.db.name
