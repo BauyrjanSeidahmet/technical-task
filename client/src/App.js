84fd418d64749630151e8f80b24b92e111887159
BauyrjanSeidakhmet <bauke.10.bs@gmail.com>
2021-11-15 15:21:43 +0600

    added private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import {Routes, Route} from "react-router-dom";
+import { useSelector } from 'react-redux';
+import {Routes, Route, Navigate, Outlet} from "react-router-dom";
 import Layout from './components/Layout/Layout';
 import Login from './containers/Login/Login';
 import Main from './containers/Main/Main';
 import Register from './containers/Register/Register';
 
-// const ProtectedRoute = ({isAllowed, redirectTo, ...props}) => {
-//   return isAllowed ?
-//       <Route {...props} /> :
-//       <Redirect to={redirectTo}/>
-// };
+const ProtectedRoute = ({isAllowed, redirectTo, ...props}) => {
+  return isAllowed ?
+      <Route {...props} /> :
+      <Navigate to={redirectTo}/>
+};
+
 
 function App() {
+  const user = useSelector(state => state.users.user);
+
+  const PrivateRoute = () => {
+
+    return user ? <Outlet /> : <Navigate to="/login" />;
+  }
 
   return (
     <Layout>
         <Routes>
-            <Route path='/' element={<Main/>}/>
             <Route exact path='/register' element={<Register/>}/>
             <Route exact path='/login' element={<Login/>}/>               
-                  {/* <ProtectedRoute
-                        isAllowed={user}
-                        redirectTo={"/login"}
-                        path="/add"
-                        exact
-                        component={AddProduct}
-                  />     */}
+            <Route exact path='/' element={<PrivateRoute/>}>
+              <Route exact path='/' element={<Main/>}/>
+            </Route>  
         </Routes>
     </Layout>
   );
