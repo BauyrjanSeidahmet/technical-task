751d8d4ee25d4130b7f3710c36cc179a91257378
BauyrjanSeidakhmet <bauke.10.bs@gmail.com>
2021-11-15 16:10:54 +0600

    input and button were created

---
+++ client/src/components/InputItem/InputItem.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function FullWidthTextField({OnChangeInput}) {
+
+  return (
+    <Box
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+      }}
+    >
+      <TextField fullWidth label="ID of User" id="fullWidth" onChange ={(e) => OnChangeInput(e)}/>
+    </Box>
+  );
+}
--- client/src/containers/Main/Main.js
+++ client/src/containers/Main/Main.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import User from '../../components/User/User';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../../store/actions/usersActions';
+import FullWidthTextField from '../../components/InputItem/InputItem';
+import { Button } from '@material-ui/core';
+import './Main.css';
 
 const Main = () => {
   const users = useSelector(state => state.users.users);
@@ -11,15 +14,27 @@ const Main = () => {
     dispatch(fetchUsers())
   }, [])
 
+  const OnChangeInput = (e) => {
+    console.log('value', e.target.value)
+}
+
   return (
     <div className="Main">
+      <div className='search'>
+        <FullWidthTextField
+          OnChangeInput={OnChangeInput}
+        />
+      <Button className="btn" variant="contained">Search</Button>
+      </div>
+      <div>
         {users?.map(user => {
-         return <User
-            key = {user.id}
-            email = {user.email}
-            age = {user.age}
-          />
-        })}
+          return <User
+              key = {user.id}
+              email = {user.email}
+              age = {user.age}
+            />
+          })}
+      </div>
     </div>
   );
 }
